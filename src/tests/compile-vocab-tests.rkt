06cacd9671dd60649c418243665ec8f1681fed7e
Theophilos Giannakopoulos <tgiannak@alum.wpi.edu>
2012-04-19 17:05:34 -0400

    bugfix and added license files

--- src/tests/compile-vocab-tests.rkt
+++ src/tests/compile-vocab-tests.rkt
@@ -19,25 +19,24 @@
     ;; only one function is complicated enough that it can fail on its own
     ;; it's not exported, so we test it via the top level
     (test-case "constraints->constants"
-      (check-true
-       (m-vocabulary-equal?
-        (constraints->vocab "foo" some-constraints)
-        (m-vocabulary
-         "foo"
-         (make-hash
-          (list
-           (cons "Role" (m-type "Role" empty))
-           (cons "Perm" (m-type "Perm" empty))
-           (cons "User" (m-type "User" empty))))
-         (make-hash
-          (list
-           (cons "ua" (m-predicate "ua" (list "User" "Role")))
-           (cons "ra" (m-predicate "ra" (list "Role" "Perm")))))
-         (make-hash
-          (list
-           (cons "b1" (m-constant "b1" "Perm"))
-           (cons "b2" (m-constant "b2" "Perm"))
-           (cons "a4" (m-constant "a4" "Role"))))
-         (make-hash)))))))
+      (check m-vocabulary-equal?
+             (constraints->vocab "foo" some-constraints)
+             (m-vocabulary
+              "foo"
+              (make-hash
+               (list
+                (cons "Role" (m-type "Role" empty))
+                (cons "Perm" (m-type "Perm" empty))
+                (cons "User" (m-type "User" empty))))
+              (make-hash
+               (list
+                (cons "ua" (m-predicate "ua" (list "User" "Role")))
+                (cons "ra" (m-predicate "ra" (list "Role" "Perm")))))
+              (make-hash
+               (list
+                (cons "b1" (m-constant "b1" "Perm"))
+                (cons "b2" (m-constant "b2" "Perm"))
+                (cons "a4" (m-constant "a4" "Role"))))
+              (make-hash))))))
 
-(run-tests compile-vocab-tests)
+;(run-tests compile-vocab-tests)
